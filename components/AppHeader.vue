39428b09faa07e96e56a866acdec56967923ad2f
Raza Poonja <razapoonja@gmail.com>
2020-02-03 02:12:25 +0500

    added navbar

---
+++ components/AppHeader.vue
@@ -0,0 +1,41 @@
+<template>
+  <header>
+    <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+      <div class="container">
+        <nuxt-link class="navbar-brand" to="/"></nuxt-link>
+        <button
+          class="navbar-toggler"
+          type="button"
+          data-toggle="collapse"
+          data-target="#navbarsExample07"
+          aria-controls="navbarsExample07"
+          aria-expanded="false"
+          aria-label="Toggle navigation"
+        >
+          <span class="navbar-toggler-icon"></span>
+        </button>
+
+        <div class="collapse navbar-collapse" id="navbarsExample07">
+          <ul class="navbar-nav mr-auto">
+            <li class="nav-item active">
+              <nuxt-link class="nav-link" to="/">
+                Home
+                <span class="sr-only">(current)</span>
+              </nuxt-link>
+            </li>
+            <li class="nav-item">
+              <nuxt-link class="nav-link" to="/jokes">Jokes</nuxt-link>
+            </li>
+            <li class="nav-item">
+              <nuxt-link class="nav-link" to="/about">About Us</nuxt-link>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </nav>
+  </header>
+</template>
+
+<script>
+export default {};
+</script>
---
+++ pages/about.vue
@@ -0,0 +1,24 @@
+<template>
+    <div>
+        <h1>About Us</h1>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nihil, laborum. Enim consectetur optio earum eaque blanditiis culpa. 
+            Accusantium delectus architecto et molestiae excepturi, earum obcaecati, doloribus, ratione provident quas exercitationem?</p>
+    </div>
+</template>
+
+<script>
+    export default {
+        head() {
+            return {
+                title: 'About Us',
+                meta: [
+                    {
+                        hid: 'description',
+                        name: 'description',
+                        content: 'Lorem ipsum dolor sit amet consectetur adipisicing elit'
+                    }
+                ]
+            }
+        }
+    }
+</script>
